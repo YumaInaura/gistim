d989a1dac6e386fa42ce3cafa9763588168dfbde
Yumainaura <yuma.inaura@gmail.com>
2018-08-15 08:00:45 +0900

    Fix Gistim::Clone

--- lib/gistim/create.rb
+++ lib/gistim/create.rb
@@ -4,24 +4,50 @@ module Gistim
       @alias_name = alias_name
     end
   
-    attr_writer :alias_name
-  
+    attr_reader :alias_name
+ 
     def create
-      File.write('README.md', 'Wellcome to Gist! ( created by gistim )')
-      self.gist_url = `gist README.md`
-      seff.gist_hash = gist_url.chomp.gsub(/\A.+\//, '')
-      clone_directory = self.alias_name || gist_hash
-      `gistim clone #{gist_url} #{clone_directory}`
-      File.delete('README.md')
-      File.write("#{clone_directory}/.gist.url", gist_url)
-      File.write("#{clone_directory}/.gist.hash", gist_hash)
+      File.write(initialize_file_path, 'Wellcome to Gist! ( created by gistim )')
+
+      create_empty
+      clone
+
+      File.delete(initialize_file_path)
+ 
+      # FIME: write good structured work flow and good test
+      # File.write("#{clone_directory}/.gist.url", gist_url)
+      # File.write("#{clone_directory}/.gist.hash", gist_hash)
+
       self
+    end
+
+    def gist_hash
+      gist_url.chomp.gsub(/\A.+\//, '')
     end
   
     private
-  
-    attr_accessor :gist_url, :gist_hash
-  
+ 
+    def clone
+      Clone.clone(gist_url, clone_directory: clone_directory)
+    end
+
+    def create_empty
+      # Execute `$ gist [some_file]` command and get gist url result
+      @gist_url ||= `gist #{initialize_file_path}`.chomp
+    end
+
+    def gist_url
+      @gist_url ||= create_empty
+    end
+
+    def initialize_file_path
+      '.gistim.tmp'
+    end
+
+    def clone_directory
+      alias_name || gist_hash
+    end
+
     # public
     class << self
       def implement
