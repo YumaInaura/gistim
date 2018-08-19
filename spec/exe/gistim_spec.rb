d24aa15d8bd262572aa70961a53b60726f6fa8cb
Yumainaura <yuma.inaura@gmail.com>
2018-08-20 08:31:20 +0900

    Add gistim --version option

--- spec/exe/gistim_spec.rb
+++ spec/exe/gistim_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper'
 
 RSpec.describe do
-  subject { `gistim --help` }
-  it { is_expected.to include 'gistim' }
+  describe '--help' do
+    subject { `gistim --help` }
+    it { is_expected.to include 'gistim' }
+  end
+
+  describe '--version' do
+    subject { `gistim --version`.chomp }
+    it { is_expected.to eq Gistim::VERSION }
+  end
 end
