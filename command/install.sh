ba4d69ade719d79b11d5f596f06fdf9f63f885c6
Yumainaura <yuma.inaura@gmail.com>
2018-08-23 04:51:14 +0900

    Fix gistimnow installer

--- command/install.sh
+++ command/install.sh
@@ -2,29 +2,36 @@
 
 readonly command_name=gistimnow
 readonly compress_file_name=install.tar.gz
-readonly github_code_base_url='https://raw.githubusercontent.com/YumaInaura/gistim/master/command/'"$compress_file_name"
+readonly compress_file_url=https://raw.githubusercontent.com/YumaInaura/gistim/master/command/"$compress_file_name"
+readonly command_home="$HOME"/"$command_name"
+readonly download_tmp_dir="$HOME"/."$command_name"_install
+
+function remove_tmp {
+  # !PREVENT WRONG REMOVING! Not use variable like `rm -rf "download_tmp_dir"`
+  rm -f "$HOME"/"$command_name"/"$compress_file_name"
+  rm -rf "$HOME"/."$command_name"_install 
+}
+trap remove_tmp EXIT
+
+readonly download_header=$(curl --head "$download_tmp_dir"/"$compress_file_name" "$compress_file_url"/"$compress_file_name"?$(date +%s))
+readonly download_ok=$(echo "$download_header" | grep -e 'HTTP .+ 200 OK')
+[[ -z "$download_ok" ]] && echo "Download file not found" && echo "$compress_file_url" && exit 1
 
 # Create home dir
-readonly command_home="$HOME"/"$command_name"
 mkdir -p "$command_home"
-
-readonly download_tmp_dir="$HOME"/."$command_name"_install
 mkdir -p "$download_tmp_dir"
-curl -o "$download_tmp_dir"/"$compress_file_name" "$github_code_base_url"/"$compress_file_name"?$(date +%s)
+curl -o "$download_tmp_dir"/"$compress_file_name" "$compress_file_url"/"$compress_file_name"?$(date +%s)
 
 # Refresh libraries
 cp "$download_tmp_dir"/"$compress_file_name" "$HOME"/"$command_name"/
-pushd "$HOME"/"$command_name"/
+pushd "$command_home"/
   tar -xzf "$compress_file_name"
 popd
-rm -f "$HOME"/"$command_name"/"$compress_file_name"
 
 # Link command to PATH
 readonly command_path=/usr/local/bin/"$command_name"
 rm -f "$command_path"
 ln -s "$command_home"/bin/"$command_name" "$command_path"
-
-rm -rf "$HOME"/."$command_name"_install # !PREVENT WRONG REMOVING! Not use variable like `rm -rf "download_tmp_dir"`
 
 echo "installed"
 
